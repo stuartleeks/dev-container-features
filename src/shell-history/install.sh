61fb446ce3433aa4529a4acba9b6ffa31b9e1b0c
Stuart Leeks <stuart@leeks.net>
2022-11-02 10:36:36 +0000

    Revert removal of sudo calls in .bashrc etc
    The calls to sudo in .bashrc etc are required as the mounted volume
    is inaccessible to non-root users

--- src/shell-history/install.sh
+++ src/shell-history/install.sh
@@ -22,6 +22,7 @@ fi
 cat << EOF >> "$_REMOTE_USER_HOME/.bashrc"
 export HISTFILE=/dc/shellhistory/.bash_history
 export PROMPT_COMMAND='history -a'
+sudo chown -R $_REMOTE_USER /dc/shellhistory
 EOF
 chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.bashrc
 
@@ -29,6 +30,7 @@ chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.bashrc
 cat << EOF >> "$_REMOTE_USER_HOME/.zshrc"
 export HISTFILE=/dc/shellhistory/.zsh_history
 export PROMPT_COMMAND='history -a'
+sudo chown -R $_REMOTE_USER /dc/shellhistory
 EOF
 chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.zshrc
 
@@ -44,5 +46,6 @@ if test -f \$history_location
     mv \$history_location "\$history_location-old"
 end
 ln -s /dc/shellhistory/fish_history \$history_location
+sudo chown -R $_REMOTE_USER \$history_location
 EOF
 chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.config/
