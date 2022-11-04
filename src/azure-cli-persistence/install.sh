ecd41ef23c5724dca7fcf76c21b039eb56a84c4d
Stuart Leeks <stuart@leeks.net>
2022-11-04 13:39:48 +0000

    Add azure-cli-persistence

---
+++ src/azure-cli-persistence/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'azure-cli-persistence'"
+echo "User: ${_REMOTE_USER}     User home: ${_REMOTE_USER_HOME}"
+
+if [  -z "$_REMOTE_USER" ] || [ -z "$_REMOTE_USER_HOME" ]; then
+  echo "***********************************************************************************"
+  echo "*** Require _REMOTE_USER and _REMOTE_USER_HOME to be set (by dev container CLI) ***"
+  echo "***********************************************************************************"
+  exit 1
+fi
+
+ln -s /dc/azure/ "$_REMOTE_USER_HOME/.azure"
+chown -R "${_REMOTE_USER}:${_REMOTE_USER}" "$_REMOTE_USER_HOME/.azure"
+
+# chown mount (only attached on startup)
+cat << EOF >> "$_REMOTE_USER_HOME/.bashrc"
+sudo chown -R "${_REMOTE_USER}:${_REMOTE_USER}" /dc/azure
+EOF
+chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.bashrc
