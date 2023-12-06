565793ca2597d6426bc7f23245606eb9bc823b25
Stuart Leeks <stuart@leeks.net>
2023-12-06 10:38:56 +0000

    Move comment from devcontainer-feature.json

--- src/dev-tunnels/install.sh
+++ src/dev-tunnels/install.sh
@@ -8,3 +8,15 @@ echo "User: ${_REMOTE_USER}     User home: ${_REMOTE_USER_HOME}"
 # curl -sL https://aka.ms/DevTunnelCliInstall | bash
 # Run that as the remote user
 su ${_REMOTE_USER} -c "curl -sL https://aka.ms/DevTunnelCliInstall | bash"
+
+
+
+# NOTE for devcontainer-feature.json
+# TODO - once devtunnel supports writing auth to HOME dir, set up a mount and symlink to persist login across dev container rebuilds
+# "mounts": [
+#     {
+#         "source": "${devcontainerId}-devtunnels",
+#         "target": "/dc/devtunnels",
+#         "type": "volume"
+#     }
+# ]
