756d538fd9c757c2dce7cdccca1785f3c627c87d
Stuart Leeks <stuart@leeks.net>
2025-03-12 11:15:49 +0000

    Handle no sudo in Add-host feature

--- src/add-host/install.sh
+++ src/add-host/install.sh
@@ -18,6 +18,10 @@ HOST_NAME=${HOST_NAME:-"host.docker.internal"}
 
 
 echo "Adding $HOST_NAME to /etc/hosts_temp pointing to $HOST_IP..."
-echo "$HOST_IP $HOST_NAME" | sudo tee -a /etc/hosts_temp > /dev/null
+if [[ $(command -v sudo > /dev/null; echo $?) == 0 ]]; then
+    echo "$HOST_IP $HOST_NAME" | sudo tee -a /etc/hosts_temp > /dev/null
+else
+    echo "$HOST_IP $HOST_NAME" >> /etc/hosts_temp
+fi
 echo "Done!"
 
