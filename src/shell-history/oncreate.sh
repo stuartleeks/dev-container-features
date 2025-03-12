4be4270bdef3917d9e95e087ec226b24f3aa9690
Stuart Leeks <stuart@leeks.net>
2025-03-12 15:58:01 +0000

    Revert to setting HIST_FILE via bashrc etc

--- src/shell-history/oncreate.sh
+++ src/shell-history/oncreate.sh
@@ -16,40 +16,44 @@ fix_permissions() {
 }
 
 # Generally, the installation favours configuring a shell to use the mounted volume
-# for storing history rather than symlinking as any operation to recreate files 
-# could delete the symlink. 
+# for storing history rather than symlinking as any operation to recreate files
+# could delete the symlink.
 # On shells where this isn't possible, we symlink as a fallback.
 
 echo "Activating feature 'shell-history'"
 echo "User: $(id -un)"
 
-if [ "$SHELL" = "/bin/bash" ]; then
-    if [[ -z "$HISTFILE_OLD" ]]; then
-        export HISTFILE_OLD=$HISTFILE
-    fi
-    export HISTFILE=/dc/shellhistory/.bash_history
-    export PROMPT_COMMAND='history -a'
-    fix_permissions /dc/shellhistory
 
-elif [ "$SHELL" = "/bin/zsh" ]; then
-
-    export HISTFILE=/dc/shellhistory/.zsh_history
-    export PROMPT_COMMAND='history -a'
-    fix_permissions /dc/shellhistory
-
-elif [ "$SHELL" = "/usr/bin/fish" ]; then
-
-    if [ -z "$XDG_DATA_HOME" ]; 
-    then
-        set history_location ~/.local/share/fish/fish_history
-    else
-        set history_location $XDG_DATA_HOME/fish/fish_history
-    fi
-
-    if [ -f $history_location ]; then
-        mv $history_location "$history_location-old"
-    fi 
-
-    ln -s /dc/shellhistory/fish_history $history_location
-    fix_permissions $history_location
+# Set HISTFILE for bash
+cat << EOF >> "$_REMOTE_USER_HOME/.bashrc"
+if [[ -z "$HISTFILE_OLD" ]]; then
+    export HISTFILE_OLD=$HISTFILE
 fi
+export HISTFILE=/dc/shellhistory/.bash_history
+export PROMPT_COMMAND='history -a'
+EOF
+
+# Set HISTFILE for zsh
+cat << EOF >> "$_REMOTE_USER_HOME/.zshrc"
+export HISTFILE=/dc/shellhistory/.zsh_history
+export PROMPT_COMMAND='history -a'
+EOF
+
+# Create symlink for fish
+mkdir -p $_REMOTE_USER_HOME/.config/fish
+cat << EOF >> "$_REMOTE_USER_HOME/.config/fish/config.fish"
+if [ -z "$XDG_DATA_HOME" ];
+then
+    set history_location ~/.local/share/fish/fish_history
+else
+    set history_location $XDG_DATA_HOME/fish/fish_history
+fi
+
+if [ -f $history_location ]; then
+    mv $history_location "$history_location-old"
+fi
+
+ln -s /dc/shellhistory/fish_history $history_location
+EOF
+
+fix_permissions /dc/shellhistory
