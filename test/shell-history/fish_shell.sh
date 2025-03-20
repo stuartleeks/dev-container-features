b5cea4b3efeab2a8d3bf2400767a742fa93f2195
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-03-20 09:05:01 +0000

    test(shell-history): more tests for fish

--- test/shell-history/fish_shell.sh
+++ test/shell-history/fish_shell.sh
@@ -6,5 +6,6 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "check fish config script" fish -c "source $HOME/.config/fish/config.fish"
+check "check fish config script validity" fish -c "source $HOME/.config/fish/config.fish"
+check "check fish config script content" bash -c "cat $HOME/.config/fish/config.fish | grep -q 'XDG_DATA_HOME'"
 check "cache dir permission" bash -c "test -w /dc/shellhistory"
